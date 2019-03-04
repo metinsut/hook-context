65c679320ac507571e0779f20f011902d8018dfc
metin_sut <metin.sut@beyn.com.tr>
2019-03-04 19:04:40 +0300

    suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import Count from './Components/Count/Count';
-import TodoList from './Components/TodoList/TodoList';
-import YoutubeTrends from './Components/YoutubeTrends/YoutubeTrends';
 import Header from './Components/Common/HeaderComponent';
+import Home from './Components/Home/Home';
 import { Main, SiteWrapper } from './style/pages/index';
-import YoutubePlayer from './Components/YoutubeTrends/youtubePlayer';
 import * as R from './Const/routes';
+const Count = lazy(() => import('./Components/Count/Count'));
+const TodoList = lazy(() => import('./Components/TodoList/TodoList'));
+const YoutubeTrends = lazy(() => import('./Components/YoutubeTrends/YoutubeTrends'));
+const YoutubePlayer = lazy(() => import('./Components/YoutubeTrends/youtubePlayer'));
+
 
 const App = () => (
    <Main>
       <Header />
-      <SiteWrapper>
-         <Switch>
-            <Route exact path={R.BASE} component={Home} />
-            <Route path={R.COUNT} component={Count} />
-            <Route path={R.TODOS} component={TodoList} />
-            <Route exact path={R.YOUTUBE} component={YoutubeTrends} />
-         </Switch>
-         <Route path={R.YOUTUBE_ID} component={YoutubePlayer} />
-      </SiteWrapper>
+      <Suspense fallback={<div>Loading...</div>}>
+         <SiteWrapper>
+            <Switch>
+               <Route exact path={R.BASE} component={Home} />
+               <Route path={R.COUNT} component={Count} />
+               <Route path={R.TODOS} component={TodoList} />
+               <Route exact path={R.YOUTUBE} component={YoutubeTrends} />
+            </Switch>
+            <Route path={R.YOUTUBE_ID} component={YoutubePlayer} />
+         </SiteWrapper>
+      </Suspense>
    </Main>
 );
 
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -6,12 +6,13 @@ import * as Todo from '../../style/pages/todos';
 
 const TodoList = () => {
    const { todos } = useContext(GlobalContext);
+
    return (
       <Todo.TodoRoot>
          <h1>Todos</h1>
          <Todo.TodoWrapper>
             <TodoAdd />
-            {todos && todos.stateTodos.todos.map((item, key) => <TodoItem key={key} item={item} index={key}/>)}
+            {todos && todos.stateTodos.todos.map((item, key) => <TodoItem key={key} item={item} index={key} />)}
          </Todo.TodoWrapper>
       </Todo.TodoRoot>
    );
